9fc958fa6a7362b94e2631fb95406f3fb6095d50
taehyundev <K.anon.11107@gmail.com>
2020-01-30 18:23:13 +0900

    for&while사용법

--- Chapter02/Py02_03.py
+++ Chapter02/Py02_03.py
@@ -2,4 +2,3 @@ for i in range(1,int(input())+1 ):
     print(i)
 
 #1부터 n까지 출력
-    
--- Chapter02/Py02_09.py
+++ Chapter02/Py02_09.py
@@ -4,4 +4,3 @@ for i in range(1, n+1):
     for j in range(1,10):
         print(i ," * ", j ," = ", (i*j))
     print()
-    
