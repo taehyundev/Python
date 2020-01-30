9cf74f39e407f887ee5f68ea712008758a144d09
taehyundev <K.anon.11107@gmail.com>
2020-01-30 15:52:18 +0900

    Chap01_print&Input

---
+++ Chapter01/Py01_05.py
@@ -0,0 +1,22 @@
+add = 3 + 25 
+#더하기
+sub = 45 - 13 
+#더하기
+mul = 9 * 9 
+#곱하기
+div = 3 / 2 
+#나누기
+div_int = 3 // 2 
+#몫
+exp = 10 ** 3 
+#제곱
+mod = 101 % 10 
+#나머기
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(div_int)
+print(exp)
+print(mod)
